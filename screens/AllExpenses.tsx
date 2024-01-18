7d71f6c2500d0ee51f094fd69c251bc3accb5562
HIT-Serge <serge.groen@heldeninit.nl>
2024-01-18 16:26:14 +0100

    new project

---
+++ screens/AllExpenses.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export function AllExpenses() {
+    // Your code here
+    return (
+        <View >
+            <Text>AllExpenses</Text>
+        </View>
+    );
+}
---
+++ screens/index.ts
@@ -0,0 +1,5 @@
+// ./screens/index.ts
+export { RecentExpenses } from './RecentExpenses';
+export { AllExpenses } from './AllExpenses';
+export { ManageExpense } from './ManageExpense';
+
